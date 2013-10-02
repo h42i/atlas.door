b7b2eeb381cf2d25ef53685576a672602cf8659d
danb <daniel@localhost.localdomain>
2013-10-02 17:29:54 +0200

    And another one.

--- hold_pin.rb
+++ hold_pin.rb
@@ -7,8 +7,13 @@ sleep_time = ARGV[1].to_i
 begin
   file = File.open(device, "w")
   file.write(pin + "=1\r\n")
+  file.close
+  
   sleep(sleep_time)
+  
+  file = File.open(device, "w")
   file.write(pin + "=0\r\n")
+  file.close
 rescue IOError => e
   # bad.
 ensure
