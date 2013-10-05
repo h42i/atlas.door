dc2a3534dd0f56fb7dc909383051d6e5514b2ba2
danb <daniel@localhost.localdomain>
2013-10-05 23:33:49 +0200

    Should work nao.

--- server.rb
+++ server.rb
@@ -3,9 +3,11 @@ require 'sinatra'
 get '/:argument' do
   begin
     open_time = params[:argument].to_i
+    
     if open_time <= 20
-      system("/home/hasi/atlas.door/open_door_downstairs.sh" + open_time.to_s)
+      system("sh", "/home/hasi/atlas.door/open_door_downstairs.sh", open_time.to_s)
     end
+    
     "Door downstairs is openinig for " + open_time.to_s + " seconds now."
   rescue
     "You're doing it wrong."
