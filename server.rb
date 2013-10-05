5477e30a788bcb94d6937346cdca1eec1163889a
danb <daniel@localhost.localdomain>
2013-10-05 20:19:16 +0200

    leon sagt er fände meine commits verstörend.... pf.

--- server.rb
+++ server.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 
-get '/downstairs' do
-  system("/home/hasi/atlas.door/open_door_downstairs.sh 3")
+get '/:argument' do
+  begin
+    open_time = params[:argument].to_i
+    if open_time <= 20
+      system("/home/hasi/atlas.door/open_door_downstairs.sh" + params[:argument])
+    end
+  rescue
+    "You're doing it wrong."
+  end
 end
 
