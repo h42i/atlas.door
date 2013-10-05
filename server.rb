3e439ef95c13c79e2c231c013923adaa831bddd0
danb <daniel@localhost.localdomain>
2013-10-05 20:45:51 +0200

    ich hasse commit messages

--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ get '/:argument' do
     if open_time <= 20
       system("/home/hasi/atlas.door/open_door_downstairs.sh" + params[:argument])
     end
+    "Door downstairs is openinig for " + open_time.to_s + " seconds now."
   rescue
     "You're doing it wrong."
   end
