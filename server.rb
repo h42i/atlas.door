44f573396c43e5bfeb596d7d0ecb9ddbeff30644
danb <daniel@localhost.localdomain>
2013-10-05 22:09:28 +0200

    Meh...

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ get '/:argument' do
   begin
     open_time = params[:argument].to_i
     if open_time <= 20
-      system("/home/hasi/atlas.door/open_door_downstairs.sh" + params[:argument])
+      system("/home/hasi/atlas.door/open_door_downstairs.sh" + open_time.to_s)
     end
     "Door downstairs is openinig for " + open_time.to_s + " seconds now."
   rescue
